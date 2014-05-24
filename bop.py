e5a9b87768c67004ff34b96248db452ac842f60a
BoppreH <boppreh@gmail.com>
2014-05-24 03:10:21 -0300

    First working version

--- bop.py
+++ bop.py
@@ -3,14 +3,40 @@ sys.path.append('../server-sent-events')
 from sse import Publisher
 
 import flask
-from collections import defaultdict
 from urllib.parse import quote
 from uuid import uuid4
 
-def start():
+class Page(object):
+    def __init__(self, publisher, data=None):
+        self.publisher = publisher
+        self.data = data or {}
+
+    def __setitem__(self, element_id, value):
+        self.data[element_id] = value
+        event = '{} {}'.format(element_id, quote(value))
+        self.publisher.publish(event)
+
+    def __getitem__(self, element_id):
+        return self.data[element_id]
+
+
+def start(html, user_cls):
     app = flask.Flask(__name__)
 
-    publisher_by_user = defaultdict(Publisher)
+    user_by_id = {}
+
+    def get_user():
+        userid = flask.request.cookies.get('userid') or str(uuid4())
+
+        try:
+            return user_by_id[userid]
+        except KeyError:
+            user = user_cls()
+            user.publisher = Publisher()
+            user.id = userid
+            user_by_id[userid] = user
+            return user
+
 
     @app.route('/sse.js')
     def server_sse():
@@ -19,36 +45,20 @@ def start():
 
     @app.route('/')
     def root():
-        try:
-            userid = flask.request.cookies['userid']
-        except KeyError:
-            userid = str(uuid4())
+        return html
 
-        resp = flask.make_response("""
-    <html>
-        <body>
-            <script src="/sse.js"></script>
-
-            <div id="log"></div>
-
-            <button onclick="post('/action')">Do action</button>
-        </body>
-    </html>
-    """)
-        resp.set_cookie('userid', userid)
-        return resp
-
-    @app.route('/subscribe')
+    @app.route('/sse/subscribe')
     def subscribe():
-        userid = flask.request.cookies['userid']
-        return flask.Response(publisher_by_user[userid].subscribe(),
-                              content_type='text/event-stream')
+        user = get_user()
+        response = flask.Response(user.publisher.subscribe(),
+                                  content_type='text/event-stream')
+        if flask.request.cookies.get('userid') is None:
+            response.set_cookie('userid', user.id)
+        return response
 
-    @app.route('/action', methods=['POST'])
-    def action():
-        userid = flask.request.cookies['userid']
-        event = 'log ' + quote('<h1>Test!</.h1')
-        publisher_by_user[userid].publish(event)
-        return ''
+    @app.route('/<method>', methods=['POST'])
+    def action(method):
+        user = get_user()
+        return getattr(user, method)(Page(user.publisher)) or ''
 
     app.run(debug=True, threaded=True)
--- sample.py
+++ sample.py
@@ -1,3 +1,20 @@
+import random
 import bop
 
-bop.start()
+class User(object):
+    def count(self, page):
+        page['log'] = '<h1>{}</h1>'.format(random.randint(1, 100))
+
+html = """
+<html>
+    <body>
+        <script src="/sse.js"></script>
+
+        <div id="log"></div>
+
+        <button onclick="call('count')">New random!</button>
+    </body>
+</html>
+"""
+
+bop.start(html, User)
